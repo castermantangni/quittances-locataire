059e4aa885e6dbcd672a4079fded387104f41d1c
castermantangni <121863705+castermantangni@users.noreply.github.com>
2026-02-06 22:27:38 +0100

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIza...ton_api_key...",
+  authDomain: "xxx.firebaseapp.com",
+  projectId: "xxx",
+  storageBucket: "xxx.firebasestorage.app",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef",
+  measurementId: "G-XXXXXXX",
+};

0bbe7af7cf77375daa3aae938fc19805ed378ee7
castermantangni <121863705+castermantangni@users.noreply.github.com>
2026-02-06 22:41:03 +0100

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,9 +1,9 @@
 window.FIREBASE_CONFIG = {
-        apiKey: "AIzaSyC5ukxwBWQgmC4k3-JOeJ0XlS_8ksXAZdk",
-        authDomain: "gestionlocative-be708.firebaseapp.com",
-        projectId: "gestionlocative-be708",
-        storageBucket: "gestionlocative-be708.firebasestorage.app",
-        messagingSenderId: "125095758052",
-        appId: "1:125095758052:web:2998efa253a1b37d6844f1",
-        measurementId: "G-7T2RL90FGB"
-     };
+  apiKey: "AIzaSyC5ukxwBWQgmC4k3-JOeJ0XlS_8ksXAZdk",
+  authDomain: "gestionlocative-be708.firebaseapp.com",
+  projectId: "gestionlocative-be708",
+  storageBucket: "gestionlocative-be708.firebasestorage.app",
+  messagingSenderId: "125095758052",
+  appId: "1:125095758052:web:2998efa253a1b37d6844f1",
+  measurementId: "G-7T2RL90FGB"
+};

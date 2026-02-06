5fb78191dc76645d359060742a377653698a3442
castermantangni <121863705+castermantangni@users.noreply.github.com>
2026-02-06 22:29:52 +0100

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,9 +1,9 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "AIza...ton_api_key...",
-  authDomain: "xxx.firebaseapp.com",
-  projectId: "xxx",
-  storageBucket: "xxx.firebasestorage.app",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef",
-  measurementId: "G-XXXXXXX",
-};
+        apiKey: "AIzaSyC5ukxwBWQgmC4k3-JOeJ0XlS_8ksXAZdk",
+        authDomain: "gestionlocative-be708.firebaseapp.com",
+        projectId: "gestionlocative-be708",
+        storageBucket: "gestionlocative-be708.firebasestorage.app",
+        messagingSenderId: "125095758052",
+        appId: "1:125095758052:web:2998efa253a1b37d6844f1",
+        measurementId: "G-7T2RL90FGB"
+     };
